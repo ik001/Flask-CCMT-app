52e52ad59aa026d43449b4da866ea3340e7c608f
Indrajeet Kumar <105151625+ik001@users.noreply.github.com>
2026-03-05 23:18:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -118,7 +118,16 @@ def sitemap():
     xml = render_template("sitemap.xml")
     return Response(xml, mimetype="application/xml")
 
+@app.route('/robots.txt')
+def robots():
+    return send_from_directory('.', 'robots.txt')
+
+@app.route('/sitemap.xml')
+def sitemap():
+    return send_from_directory('.', 'sitemap.xml')
+
 if __name__ == "__main__":
     app.run()
+
 
 
